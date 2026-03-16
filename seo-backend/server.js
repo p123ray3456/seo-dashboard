283895dfa68b06f202343e3678a8d0359d568090
Priyanshu <kumarpriyanshu30578@gmail.com>
2026-03-17 02:17:30 +0530

    fix railway port issue

--- seo-backend/server.js
+++ seo-backend/server.js
@@ -286,9 +286,11 @@ const limiter = rateLimit({
 app.use(limiter);
     /* ================= SERVER ================= */
 
-    app.listen(5000, () => {
-      console.log("🚀 Backend running on http://localhost:5000");
-    });
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
 
   } catch (error) {
     console.log("❌ MongoDB Error:", error);
