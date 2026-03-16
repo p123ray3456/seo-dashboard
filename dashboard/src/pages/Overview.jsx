8d562b677ca04db770d2d12b4f02cc8a0f4f7d0c
Priyanshu <kumarpriyanshu30578@gmail.com>
2026-03-17 04:25:53 +0530

    fix overview api

--- dashboard/src/pages/Overview.jsx
+++ dashboard/src/pages/Overview.jsx
@@ -15,7 +15,7 @@ const Overview = () => {
 
   useEffect(() => {
 
-    fetch(`http://localhost:5000/clients/${clientId}`)
+    fetch(`https://seo-dashboard-production-ec44.up.railway.app/clients/${clientId}`)
       .then(res => res.json())
       .then(data => setClient(data))
       .catch(err => console.log(err));
