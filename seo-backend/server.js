600b780509be50caead224af3aea97a0caee180a
Priyanshu <kumarpriyanshu30578@gmail.com>
2026-03-17 03:33:55 +0530

    fix mongodb connection

--- seo-backend/server.js
+++ seo-backend/server.js
@@ -52,10 +52,7 @@ app.get("/", (req, res) => {
 
 const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(uri);
 
 let db;
 
